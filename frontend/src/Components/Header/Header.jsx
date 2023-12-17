1d9f3cc3355dcc14b8e6580894dc6ec94139818f
Bohdan <bohdanmylymuk@gmail.com>
2023-12-18 00:21:59 +0200

    init project

---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Header.css"
+import logo from "../Header/images/logo.svg" 
+const Header = () => {
+    return (
+            <header className="header">
+                <div className="container">
+                    <nav className="nav">
+                        <ul className="menu">
+                            <li className="menu-item">  
+                                <a class="menu-item" href="#">What is new</a>
+                            </li>
+                            <li className="menu-item">
+                                <a class="menu-item" href="#">Color</a>
+                            </li>
+                            <li className="menu-item">
+                                <a class="menu-item" href="#">Order</a>
+                            </li>
+                            </ul>
+                        <a className="logo">
+                            <img class="logo-img" src={logo} alt="logo" />
+                        </a>
+                        <a class="phone" href="tel:380731841610">+38 073-184-16-10</a>
+                    </nav>
+                </div>
+            </header>
+      );
+} 
+export default Header
